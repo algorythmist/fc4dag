93d87d79d10ea518a430293a8a2fb3f785c557e0
algorythmist <Htig3er0s!>
2020-08-17 12:58:19 -0700

    graphviz

--- src/main/kotlin/com/tecacet/fc4/GraphExporter.kt
+++ src/main/kotlin/com/tecacet/fc4/GraphExporter.kt
@@ -1,13 +1,36 @@
 package com.tecacet.fc4
 
+import guru.nidi.graphviz.attribute.Color
+import guru.nidi.graphviz.attribute.Rank
+import guru.nidi.graphviz.attribute.Shape
+import guru.nidi.graphviz.engine.Format
+import guru.nidi.graphviz.engine.Graphviz
+import guru.nidi.graphviz.model.Factory.*
+import guru.nidi.graphviz.model.Node
 import org.jgrapht.Graph
+import java.io.File
 import java.lang.StringBuilder
 
-fun toDagitty(graph: Graph<Weapon, Edge>) : String {
+fun toDagitty(graph: Graph<Weapon, Edge>): String {
     val builder = StringBuilder("dag {\n")
     graph.edgeSet().forEach {
         builder.append("\"${it.source()}\" -> \"${it.target()}\"\n")
     }
     builder.append("}")
     return builder.toString()
+}
+
+fun toGraphviz(g: Graph<Weapon, Edge>, filename: String) {
+
+    var gv = graph("test").directed()
+        .graphAttr().with(Color.AZURE) //.with(Rank.dir(Rank.RankDir.BOTTOM_TO_TOP))
+        .nodeAttr().with(Shape.BOX)
+
+    val nodes = arrayListOf<Node>()
+    g.edgeSet().forEach {
+        nodes.add(node(it.source().name).link(to(node(it.target().name))))
+    }
+    gv = gv.with(nodes)
+    Graphviz.fromGraph(gv).render(Format.PNG).toFile(File(filename))
+
 }
---
+++ src/main/kotlin/com/tecacet/fc4/Main.kt
@@ -0,0 +1,22 @@
+package com.tecacet.fc4
+
+import org.jgrapht.alg.TransitiveReduction
+import org.jgrapht.graph.DefaultEdge
+import org.jgrapht.nio.dot.DOTExporter
+import java.io.File
+
+fun main() {
+
+    fun quote(str: String) = "\"$str\""
+
+    val weapons = readWeapons("sidearms.csv")
+    val graph = buildGraph(weapons)
+    TransitiveReduction.INSTANCE.reduce(graph)
+    val dagitty = toDagitty(graph)
+    println(dagitty)
+
+    val dotExporter = DOTExporter<Weapon, Edge> { quote(it.name) }
+    dotExporter.exportGraph(graph, File("sidearms.dot"))
+    toGraphviz(graph, "sidearms.png")
+
+}
